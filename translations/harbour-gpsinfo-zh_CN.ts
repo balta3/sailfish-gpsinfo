edc65ce294cb8e472a5c0bdb3feb6ea0c22c0d92
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-11-25 20:43:13 +0800

    Create harbour-gpsinfo-zh_CN.ts

---
+++ translations/harbour-gpsinfo-zh_CN.ts
@@ -0,0 +1,428 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="47"/>
+        <source>An app to show all position information</source>
+        <translation>显示所有位置信息的软件</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="53"/>
+        <source>Version</source>
+        <translation>版本</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="65"/>
+        <source>GPSInfo is open source software licensed under the terms of the GNU General Public License.</source>
+        <translation>GPSInfo  是 GNU 通用许可协议条款下的开源软件。</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="70"/>
+        <source>View license</source>
+        <translation>查看版本</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="79"/>
+        <source>For suggestions, bugs and ideas visit </source>
+        <translation>提出建议、反馈 bug 并给予灵感</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/pages/CoverPage.qml" line="28"/>
+        <source>GPS</source>
+        <translation>GPS</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CoverPage.qml" line="31"/>
+        <source>active</source>
+        <translation>活跃</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CoverPage.qml" line="31"/>
+        <source>inactive</source>
+        <translation>不活跃</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CoverPage.qml" line="34"/>
+        <source>Latitude</source>
+        <translation>纬度</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CoverPage.qml" line="49"/>
+        <source>Longitude</source>
+        <translation>经度</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CoverPage.qml" line="64"/>
+        <source>Altitude</source>
+        <translation>海拔</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CoverPage.qml" line="79"/>
+        <source>Speed</source>
+        <translation>速度</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CoverPage.qml" line="86"/>
+        <source>m/s</source>
+        <translation>米/秒</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CoverPage.qml" line="88"/>
+        <source>km/h</source>
+        <translation>千米/时</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CoverPage.qml" line="92"/>
+        <source>ft/s</source>
+        <translation>英尺/秒</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CoverPage.qml" line="94"/>
+        <source>mph</source>
+        <translation>英里/时</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CoverPage.qml" line="102"/>
+        <source>Mov.</source>
+        <translation>Mov.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CoverPage.qml" line="114"/>
+        <source>Vert. acc.</source>
+        <translation>Vert.acc.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CoverPage.qml" line="129"/>
+        <source>Hor. acc.</source>
+        <translation>Hor.acc.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CoverPage.qml" line="144"/>
+        <source>Satel.</source>
+        <translation>Satel.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CoverPage.qml" line="150"/>
+        <source>Com.</source>
+        <translation>Com.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CoverPage.qml" line="156"/>
+        <source>Cal.</source>
+        <translation>Cal.</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="49"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="53"/>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="57"/>
+        <source>Deactivate GPS</source>
+        <translation>关闭 GPS</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="57"/>
+        <source>Activate GPS</source>
+        <translation>激活 GPS</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="74"/>
+        <source>Copy location</source>
+        <translation>复制位置</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="93"/>
+        <source>GPSInfo</source>
+        <translation>GPSInfo</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="108"/>
+        <source>GPS</source>
+        <translation>GPS</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="110"/>
+        <source>active</source>
+        <translation>激活</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="110"/>
+        <source>inactive</source>
+        <translation>未激活</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="113"/>
+        <source>Latitude</source>
+        <translation>纬度</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="127"/>
+        <source>Longitude</source>
+        <translation>经度</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="141"/>
+        <source>Altitude</source>
+        <translation>海拔</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="155"/>
+        <source>Speed</source>
+        <translation>速度</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="161"/>
+        <source>m/s</source>
+        <translation>米/秒</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="163"/>
+        <source>km/h</source>
+        <translation>千米/时</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="167"/>
+        <source>ft/s</source>
+        <translation>英尺/秒</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="169"/>
+        <source>mph</source>
+        <translation>英里/时</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="177"/>
+        <source>Movement direction</source>
+        <translation>运动方向</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="182"/>
+        <source>Last update</source>
+        <translation>最近更新</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="187"/>
+        <source>Vertical accuracy</source>
+        <translation>垂直精度</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="201"/>
+        <source>Horizontal accuracy</source>
+        <translation>水平精度</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="215"/>
+        <source>Satellites in use / view</source>
+        <translation>使用及找到的卫星</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="220"/>
+        <source>Compass direction</source>
+        <translation>罗盘方向</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="225"/>
+        <source>Compass calibration</source>
+        <translation>罗盘校正</translation>
+    </message>
+</context>
+<context>
+    <name>LicensePage</name>
+    <message>
+        <location filename="../qml/pages/LicensePage.qml" line="20"/>
+        <source>License</source>
+        <translation>许可协议</translation>
+    </message>
+</context>
+<context>
+    <name>SatelliteInfoPage</name>
+    <message>
+        <location filename="../qml/pages/SatelliteInfoPage.qml" line="15"/>
+        <source>Satellite Info</source>
+        <translation>卫星信息</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SatelliteInfoPage.qml" line="180"/>
+        <source>Satellites in use / view</source>
+        <translation>使用及找到的卫星</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="48"/>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="52"/>
+        <source>Coordinate format</source>
+        <translation>坐标格式</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="55"/>
+        <source>degree</source>
+        <translation>度</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="59"/>
+        <source>decimal</source>
+        <translation>小数</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="66"/>
+        <source>Units</source>
+        <translation>单位</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="69"/>
+        <source>metric</source>
+        <translation>公制</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="76"/>
+        <source>imperial</source>
+        <translation>英制</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="87"/>
+        <location filename="../qml/pages/SettingsPage.qml" line="224"/>
+        <source>Speed</source>
+        <translation>速度</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="91"/>
+        <source>m/s</source>
+        <translation>米/秒</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="96"/>
+        <source>km/h</source>
+        <translation>千米/时</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="101"/>
+        <source>ft/s</source>
+        <translation>英尺/秒</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="106"/>
+        <source>mph</source>
+        <translation>英里/时</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="114"/>
+        <source>Language</source>
+        <translation>语言</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="132"/>
+        <source>Update interval</source>
+        <translation>更新间隔</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="146"/>
+        <source>Rotate satellite view</source>
+        <translation>旋转卫星视图</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="149"/>
+        <source>yes</source>
+        <translation>确认</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="155"/>
+        <source>no</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="174"/>
+        <source>Show</source>
+        <translation>显示</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="180"/>
+        <source>Appview</source>
+        <translation>软件视图</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="186"/>
+        <source>Cover</source>
+        <translation>封面</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="192"/>
+        <source>GPS state</source>
+        <translation>GPS 状态</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="200"/>
+        <source>Latitude</source>
+        <translation>纬度</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="208"/>
+        <source>Longitude</source>
+        <translation>经度</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="216"/>
+        <source>Altitude</source>
+        <translation>海拔</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="232"/>
+        <source>Movement Direction</source>
+        <translation>运动方向</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="240"/>
+        <source>Last Update</source>
+        <translation>最近更新</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="248"/>
+        <source>Vertical Accuracy</source>
+        <translation>垂直精度</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="256"/>
+        <source>Horizontal Accuracy</source>
+        <translation>水平精度</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="264"/>
+        <source>Satellite Info</source>
+        <translation>卫星信息</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="272"/>
+        <source>Compass Direction</source>
+        <translation>罗盘方向</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="280"/>
+        <source>Compass Calibration</source>
+        <translation>罗盘校准</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="290"/>
+        <source>requires app restart</source>
+        <translation>需要重启软件</translation>
+    </message>
+</context>
+</TS>
